6c7df7351f546a70cf8cc1aca742e2428d33a3a7
Oscar Gonzalez <oscarg610@gmail.com>
2018-08-18 15:29:50 -0400

    Update labels, re-order fields

--- src/index.ts
+++ src/index.ts
@@ -2,35 +2,35 @@ import * as Joi from 'joi';
 
 export const companyFormSchema = Joi.object({
   name: Joi.string().required().max(100).label('Company Name'),
-  description: Joi.string().required().max(1000).tags('wysiwyg').label('description'),
   tagline: Joi.string().optional().max(150).label('Tagline'),
   url: Joi.string().uri({
     scheme: ['http', 'https'],
   }).label('Website'),
+  description: Joi.string().required().max(1000).tags('wysiwyg').label('Description'),
   // @todo validate + upload logo img
   // logoSrc: Joi.string().optional(),
-});
+}).label('About the company');
 
 export const jobFormSchema = Joi.object({
   title: Joi.string().required().max(150).label('Job Title'),
-  description: Joi.string().required().max(1000).tags('wysiwyg').label('Descrpition'),
-  instructions: Joi.string().required().max(500).tags('textarea').label('Application Instructions'),
   location: Joi.string().required().max(200).label('Location'),
   fulltime: Joi.boolean().required().tags('radio').meta({
     label: {
       0: 'Part time',
       1: 'Full time',
     },
-  }).label('Job Length'),
+  }).label('Workload'),
   contract: Joi.boolean().required().tags('radio').meta({
     label: {
       0: 'Employment',
-      1: 'Contract',
+      1: 'Contract/Freelance',
     },
   }).label('Work Type'),
   reloc: Joi.boolean().optional().default(false).tags('cb').label('Relocation Assistance'),
   visa: Joi.boolean().optional().default(false).tags('cb').label('Visa Sponsorship'),
-});
+  description: Joi.string().required().max(1000).tags('wysiwyg').label('Job Description'),
+  instructions: Joi.string().required().max(500).tags('textarea').label('Application Instructions'),
+}).label('About the job');
 
 export const userFormSchema = Joi.object({
   email: Joi.string().required().email().label('Email Address'),
