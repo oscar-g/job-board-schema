63f4282d96bb1691fafb2d9e0488f0268c6bf08b
Oscar Gonzalez <oscarg610@gmail.com>
2018-09-07 14:54:41 -0400

    fix(joi): add description when schema default is function

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,8 @@ export const userFormSchema = Joi.object({
 });
 
 export const scheduleFormSchema = Joi.object({
-  start:  Joi.date().required().min('now').default(Date.now).label('Post Date'),
+  start:  Joi.date().required().min('now').default(Date.now).label('Post Date')
+    .description('The post will go live on this day'),
   length: Joi.number().min(1).default(1).label('Running time').meta({
     optVal: {
       0: {label: '1 Month', value: 1},
